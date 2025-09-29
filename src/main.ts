ef7669f4506f89882ff8c57ccbfae6dfe345306b
Oleksii Fedorenko <oleksii.fedorenko@icecat.biz>
2025-09-29 04:40:56 +0300

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
